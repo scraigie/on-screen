068c07e0a664e8ff565a6974f411e33213d28f31
Simon Craigie <Simon.Craigie@global.com>
2019-06-16 12:25:18 +0100

    Initial commit. Added android project with modules for tv and movies

---
+++ android/app/src/main/java/uk/co/scraigie/onscreen/MainActivity.kt
@@ -0,0 +1,38 @@
+package uk.co.scraigie.onscreen
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var textMessage: TextView
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                textMessage.setText(R.string.title_home)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_dashboard -> {
+                textMessage.setText(R.string.title_dashboard)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_notifications -> {
+                textMessage.setText(R.string.title_notifications)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        nav_view?.apply {
+            setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+        }
+        textMessage = findViewById(R.id.message)
+    }
+}
